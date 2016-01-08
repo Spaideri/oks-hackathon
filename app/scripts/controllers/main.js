448358179f7eafe9692b5719b91d2f347bc20c2a
lavonardo <ville.lavonius@huimio.com>
2016-01-08 13:21:15 +0200

    Adds leaflet & very basic map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -30,6 +30,17 @@ angular.module('oksApp')
 
     console.log('foob', LocationResource);
 
+    map = new L.Map('map');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(60.1708, 24.9375),9);
+	map.addLayer(osm);
+
     LocationResource.getLocations().then(function getLocationsSuccess(data) {
       console.log('data', data);
     });
