5c86af8a748f16975782ab668bbc9cc2facfe48b
lavonardo <ville.lavonius@huimio.com>
2016-01-08 13:52:20 +0200

    Adds (temporarily hardcoded) markers for other users and a circle for the user running the app.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -32,18 +32,41 @@ angular.module('oksApp')
 
     map = new L.Map('map');
 
+    // code taken from leaflet's documentation
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});
 
-	// start the map in South-East England
-	map.setView(new L.LatLng(60.1708, 24.9375),9);
+	// start the map in Helsinki
+	map.setView(new L.LatLng(60.1708, 24.9375), 14);
 	map.addLayer(osm);
+
+    // add per-user markers & popups
+    var markerSimo = L.marker([60.1715, 24.9300]).addTo(map);
+    var markerOsmo = L.marker([60.1711, 24.9316]).addTo(map);
+    markerSimo.bindPopup("Simo - paikalla 2:35");
+    markerOsmo.bindPopup("Osmo-Inkeri - paikalla 16 sekuntia");
 
     LocationResource.getLocations().then(function getLocationsSuccess(data) {
       console.log('data', data);
     });
+
+    // Get my location and add it as a dircle
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          console.log("success: " + position.coords.latitude, position.coords.longitude);
+          var circleMe = L.circle([position.coords.latitude, position.coords.longitude], 100, {
+              color: 'red',
+              fillColor: '#f03',
+              fillOpacity: 0.5
+          }).addTo(map);
+
+          circleMe.bindPopup("Minä - paikalla nyt");
+        });
+    } else {
+        console.log("no geolocation!");
+    }
 
     $scope.foo = 'bar';
 
